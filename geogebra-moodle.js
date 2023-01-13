da089712ba1e911ff71af29d5d544af4927d5c17
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2023-01-13 18:46:04 +0100

    Update geogebra-moodle.js

--- geogebra-moodle.js
+++ geogebra-moodle.js
@@ -78,12 +78,12 @@ if (typeof window.GeogebraMoodleElements === 'undefined') {
         this.iframe.filter = 'blur(5px)'
         const successMessage = document.createElement('div');
         successMessage.textContent = 'Vous avez déjà effectué cet exercice';
-        successMessage.setAttribute('style', 'z-index: 999; position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);background-color: lightgreen;padding: 10px;border: 1px solid green;color: green;');
+        successMessage.setAttribute('style', 'position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);background-color: lightgreen;padding: 10px;border: 1px solid green;color: green;');
         this.appendChild(successMessage)
       }
       
       this.appendChild(iframe)
-      
+
       let appletOnLoad;
       if (!questionDiv.classList.contains('notyetanswered')) {
         this.afficherPopupDejaFait()
